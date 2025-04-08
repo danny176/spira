9a0a33ee428acbacaafed1cc142e308c487f806c
Mikkelbilgrav1 <eaa23msb@students.eaaa.dk>
2025-04-08 17:19:00 +0200

    ydelser og bæredygtighed godt på vej + mobil versioner

--- src/views/Ydelser.jsx
+++ src/views/Ydelser.jsx
@@ -8,24 +8,6 @@ import IPHONEJPEG from "../assets/iphonejpeg.jpeg";
 */
 import "../css-med-lille/ydelser.css";
 
-/* const ydelser = [
-  {
-    titel: "Branding & Visuel identitet",
-    beskrivelse: "Vi udarbejder moodboard sammen, så vi kan nemt kan finde frem til lige netop dine ønsker. Vi designer dit nye logo og branddesign i tæt samarbejde, for at få det bedst mulige resultat. Her efter vil vi udarbejde en designmanual, så du også nemt selv kan skabe ændringer. Derudover kan vi hjælpe dig med grafisk design til både online og offline materialer. "
-  },
-  {
-    titel: "Webudvikling & Design",
-    beskrivelse: "Vi designer og udvikler din nye hjemmeside lige efter dine ønsker - ofte i REACT eller wordpress. Vi har altid brugervenlighed i fokus, og går derfor meget op i at vide mest muligt om målgruppen, for at kunne skabe den bedst mulige brugeroplevelse. Derudover har vi stor erfaring indenfor responsivt design, så din hjemmeside vil fungere på alle enheder."
-  },
-  {
-    titel: "Websupport & Vedligeholdelse",
-    beskrivelse: "Har du allerede en hjemmeside du er rigtig glad for? Vi kan hoste hjemmesiden for dig, og hjælpe dig med vedligeholdelse af denne. Vi tilbyder at hjælpe dig med løbende optimeringer, sikkerhedsopdateringer og teknisk support, for at du selv kan spare tid, og kan fokusere mere på din kerneforretning. "
-  },
-  {
-    titel: "Webdesign & Bæredygtighed",
-    beskrivelse: "Vi tilbyder at optimere din hjemmeside, for at gøre den mere bæredygtig. Vi kan både skabe den fra bunden, med fokus på bæredygtighed, eller blot optimere din allerede eksisterende hjemmeside.  Vi gør det med fokus på et lavere CO₂-aftryk, men også fordi det faktisk vil gøre fx loading tider hurtigere, eller optimere din SEO endnu mere.Vil du vide mere om bæredygtighed indenfor den online verden? SÅ KLIK HER"
-  },
-]; */
 
 
 export default function ForwardChain() {
@@ -36,7 +18,8 @@ export default function ForwardChain() {
        <div className="ydelse-header-alt">
             <div>
               <p className="-intro-text">
-                  Her kan du læse mere om vores syn på online bæredygtighed.
+                Her kan du læse mere om, hvilke ydelser vi tilbyder. <br/> 
+                Synes du der mangler noget? Kontakt os, så skal vi nok finde ud af noget!
                 </p>
               </div>
             <div className="ydelse-header">
@@ -48,7 +31,73 @@ export default function ForwardChain() {
               </div>
             </div>
 
+            {/* Kort over ydelser samt ikon og pris */}
+            <div className='kortholder'>
+              <div className='kort'>
+                <div className='kort-tekst'>
+                <h2>Branding & Visuel Identitet</h2>
+                <p> 
+                  Vi udarbejder moodboard sammen, så vi kan nemt kan finde frem til lige netop dine ønsker
+                  Vi designer dit nye logo og branddesign i tæt samarbejde, for at få det bedst mulige resultat. 
+                  Her efter vil vi udarbejde en designmanual, så du også nemt selv kan skabe ændringer.
+                  Derudover kan vi hjælpe dig med grafisk design til både online og offline materialer.
+                </p>
+              </div>
+              <div className='kort-billede'>
+                <img src={SPIRALIKON} alt="logo"/>
+              </div>
+              </div>
 
+              <div className='kort'>
+                <div className='kort-tekst'>
+                <h2>Webudvikling & Design</h2>
+                <p> 
+                  Vi designer og udvikler din nye hjemmeside lige efter dine ønsker - ofte i REACT eller wordpress. 
+                  Vi har altid brugervenlighed i fokus, og går derfor meget op i at vide mest muligt om målgruppen, for at kunne skabe den bedst mulige brugeroplevelse.
+                  Derudover har vi stor erfaring indenfor responsivt design, så din hjemmeside vil fungere på alle enheder.
+                </p>
+              </div>
+              <div className='kort-billede'>
+                <img src={SPIRALIKON} alt="logo"/>
+              </div>
+              </div>
+
+              <div className='kort'>
+                <div className='kort-tekst'>
+                <h2>Websupport & Vedligeholdelse</h2>
+                <p> 
+                  Har du allerede en hjemmeside du er rigtig glad for?
+                  Vi kan hoste hjemmesiden for dig, og hjælpe dig med vedligeholdelse af denne.
+                  Vi tilbyder at hjælpe dig med løbende optimeringer, sikkerhedsopdateringer og teknisk support, for at du selv kan spare tid, og kan fokusere mere på din kerneforretning.
+                </p>
+              </div>
+              <div className='kort-billede'>
+                <img src={SPIRALIKON} alt="logo"/>
+              </div>
+              </div>
+
+              <div className='kort'>
+                <div className='kort-tekst'>
+                <h2>Webdesign & bæredygtighed</h2>
+                <p> 
+                  Vi tilbyder at optimere din hjemmeside, for at gøre den mere bæredygtig. Vi kan både skabe den fra bunden, med fokus på bæredygtighed, eller blot optimere din allerede eksisterende hjemmeside. 
+                  Vi gør det med fokus på et lavere CO₂-aftryk, men også fordi det faktisk vil gøre fx loading tider hurtigere, eller optimere din SEO endnu mere. 
+
+                  Vil du vide mere om bæredygtighed indenfor den online verden? SÅ KLIK HER
+                </p>
+              </div>
+              <div className='kort-billede'>
+                <img src={SPIRALIKON} alt="logo"/>
+              </div>
+              </div>
+            </div>
+
+            {/* Kontakt CTA */}
+      <div className="kontakt-cta">
+        <p>Vil du vide mere om hvad vi tilbyder af mere bæredygtigt webdesign?</p>
+        <p>Du er velkommen til at kontakte os!</p>
+        <button className="kontakt-button">Kontakt</button>
+      </div>
   </section>
     
   )
